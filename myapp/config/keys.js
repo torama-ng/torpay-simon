af6b87d163359aec9a4a7b09242d62f7c8b9bfd5
Simon Jerry <swagasoft@gmail.com>
2019-02-08 09:34:19 +0100

    torama pay nodejs version

---
+++ myapp/config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    session : {
+        sessionKey : 'thekeyisfffjdgreat'
+    },
+    mongodb :{
+        dbURL : 'mongodb://localhost:27017/payment_db'
+    },
+
+}
---
+++ myapp/models/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    hash_key : 'D3D1D05AFE42AD50818167EAC73C109168A0F108F32645C8B59E897FA930DA44F9230910DAC9E20641823799A107A02068F7BC0F4CC41D2952E249552255710F',
+    gtpay_mert_id : '17',
+    gtpay_tranx_curr : '566',
+    gtpay_echo_data : "TEST",
+    gtpay_tranx_noti_url : "http://127.0.0.1:8888/request"
+}
